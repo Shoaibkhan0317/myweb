9c48f5787f5bbd9cac6d90b7a37a0f77a55b76cf
Shoaibkhan0317 <shoaib.khan58546.com>
2023-11-17 18:16:10 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+var crsr = document.querySelector("#cursor")
+var blur = document.querySelector("#cursor-blur")
+
+document.addEventListener("mousemove",function(dets){
+   crsr.style.left = dets.x+"px"
+   crsr.style.top= dets.y+"px"
+   blur.style.left = dets.x-240+"px"
+   blur.style.top= dets.y-240+"px"
+})
+
+gsap.to("#nav",{
+  backgroundColor:"#000",
+  height:"110px",
+  duration:0.5,
+  scrollTrigger:{
+    trigger:"#nav",
+    scroller:"body",
+    markers:true,
+    start:"top -10%",
+    end:"top -11%",
+    scrub:3
+
+  }
+})
+gsap.to("#main",{
+    backgroundColor:"#000",
+    scrollTrigger:{
+       trigger:"#main",
+       scroller:"body",
+       markers:"true",
+       start:"top -25%",
+       end:"top -70%",
+       scrub:2
+
+    }
+
+
+
+})
